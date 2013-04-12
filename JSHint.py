a76a7e1a226b61688c08c1fd28099dcae1af6bd1
Jamie Hoover ⚔ <jamie@ninjaui.com>
2013-04-12 00:07:53 -0700

    Preview Image
    Branch with preview image for including in the master readme.md.

--- JSHint.py
+++
@@ -1,8 +0,0 @@
-import sublime, sublime_plugin
-
-class JshintCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    self.window.run_command('set_build_system', {
-      'file': 'Packages/JSHint/JSHint.sublime-build'
-    })
-    self.window.run_command('build')
