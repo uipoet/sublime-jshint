c203771b8a282d8aa9d00fc01e8cf0deee55faec
Jamie Hoover ⚔ <jamie@ninjaui.com>
2013-04-11 21:52:35 -0700

    Remove Unused Warning
    Removes reporting of unused variables as a warning, since these have changed in JSHint's API to be an error.
    
    Cleans up module syntax to remove unneeded strict declaration and have the more standard pattern of a public module export.

--- reporter.js
+++ reporter.js
@@ -1,88 +1,79 @@
-'use strict';
+exports.reporter = function (errors, results) {
+  var
+    errorString = ' Error',
+    warningString = ' Warning',
+    file = results[0].file,
+    errorLength, warningLength, globals, orphans;
 
-module.exports = {
-
-  reporter: function (errors, results, done) {
-
+  function numberWang(wangaNumb) {
     var
-      errorString = ' Error',
-      warningString = ' Warning',
-      file = results[0].file,
-      errorLength, warningLength, globals, orphans;
+      thatsNumberWang = 8 - wangaNumb,
+      stayNumberWang = '', i;
 
-    function numberWang(wangaNumb) {
-      var
-        thatsNumberWang = 8 - wangaNumb,
-        stayNumberWang = '', i;
-
-      for (i = 0; i < thatsNumberWang; i += 1) {
-        stayNumberWang += ' ';
-      }
-
-      return stayNumberWang;
-    }
-    var buffer = '';
-    console.log = function(){
-      var args = Array.prototype.slice.call(arguments);
-      buffer += args.join('');
-      buffer += '\n';
-    }
-    console.log('[JSHint file:', file + ']');
-
-    results.forEach(function (result) {
-
-      globals = result.implieds;
-      orphans = result.unused;
-
-      warningLength = (globals ? globals.length : 0) + (orphans ? orphans.length : 0);
-
-      if (warningLength > 1) {
-        warningString += 's';
-      }
-
-    });
-
-    if (errors) {
-      errorLength = errors.length;
-
-      if (errorLength > 0) {
-        if (errorLength > 1) {
-          errorString += 's';
-        }
-
-        console.log(' ', errorLength, errorString + ':');
-
-        errors.forEach(function (result) {
-          var error = result.error;
-
-          console.log(numberWang((error.line.toString() + error.character.toString()).length), error.line + ',' + error.character + ':', error.reason);
-        });
-      }
+    for (i = 0; i < thatsNumberWang; i += 1) {
+      stayNumberWang += ' ';
     }
 
-    if (warningLength > 0) {
-      console.log(' ', warningLength, warningString + ':');
+    return stayNumberWang;
+  }
+  var buffer = '';
 
-      if (globals) {
-        globals.forEach(function (global) {
-          for (var line in global.line){
+  console.log = function () {
+    var args = Array.prototype.slice.call(arguments);
+    buffer += args.join('');
+    buffer += '\n';
+  };
+
+  console.log('[JSHint file:', file + ']');
+
+  results.forEach(function (result) {
+
+    globals = result.implieds;
+    orphans = result.unused;
+
+    warningLength = (globals ? globals.length : 0) + (orphans ? orphans.length : 0);
+
+    if (warningLength > 1) {
+      warningString += 's';
+    }
+
+  });
+
+  if (errors) {
+    errorLength = errors.length;
+
+    if (errorLength > 0) {
+      if (errorLength > 1) {
+        errorString += 's';
+      }
+
+      console.log(' ', errorLength, errorString + ':');
+
+      errors.forEach(function (result) {
+        var error = result.error;
+
+        console.log(numberWang((error.line.toString() + error.character.toString()).length), error.line + ',' + error.character + ':', error.reason);
+      });
+    }
+  }
+
+  if (warningLength > 0) {
+    if (globals) {
+      console.log(' ', globals, warningString + ':');
+
+      globals.forEach(function (global) {
+        for (var line in global.line) {
+          if (global.line) {
             console.log(numberWang(global.line[line].toString().length + 1), global.line[line] + ',1: \'' + global.name + '\' is an implied global variable.');
           }
-        });
-      }
-
-      if (orphans) {
-        orphans.forEach(function (orphan) {
-          console.log(numberWang(orphan.line.toString().length + 1), orphan.line + ',1: \'' + orphan.name + '\' is an unused variable.');
-        });
-      }
+        }
+      });
     }
-
-    if (!errorLength && !warningLength) {
-      console.log('✓ JSHint PASSED! (no Errors or Warnings)');
-    }
-
-    process.stdout.write(buffer);
-    setTimeout('',3000);
   }
+
+  if (!errorLength && !warningLength || warningLength === 1) {
+    console.log('✓ JSHint PASSED! (no Errors or Warnings)');
+  }
+
+  process.stdout.write(buffer);
 };
