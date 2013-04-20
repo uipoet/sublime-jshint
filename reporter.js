4079d66fc8117b0a04213113afedb739fa73b955
Jamie Hoover ⚔ <jamie@ninjaui.com>
2013-04-20 14:49:48 -0700

    Buffered Output
    Windows can't handle multiple console calls and ignores after the first.
    
    Reimplementing an earlier workaround submitted by @tomgasson to buffer all the logging lines into a single string to output only once at the end.

--- reporter.js
+++ reporter.js
@@ -1,5 +1,6 @@
 exports.reporter = function (errors, results) {
   var
+    buffer = '',
     title = 'error';
 
   function numberWang(wangaNumb) {
@@ -14,7 +15,7 @@ exports.reporter = function (errors, results) {
     return stayNumberWang;
   }
 
-  console.info('[JSHint:', results[0].file + ']\n');
+  buffer += '[JSHint: ' + results[0].file + ']\n\n';
 
   if (errors.length) {
     if (errors.length > 1) {
@@ -25,12 +26,14 @@ exports.reporter = function (errors, results) {
       var
         error = result.error;
 
-      console.error(numberWang((error.line + error.character.toString()).length), error.line + ',' + error.character + ':', error.reason);
+      buffer += numberWang((error.line + error.character.toString()).length) + ' ' + error.line + ',' + error.character + ':' + ' ' + error.reason + '\n';
     });
 
-    console.error('\n ✗', errors.length, title + ', double-click above, [F4] for next, [shift-F4] for previous.\n');
+    buffer += '\n✗ ' + errors.length + ' ' + title + ', double-click above, [F4] for next, [shift-F4] for previous.\n';
 
   } else {
-    console.info(' ✓ 0 errors, [esc] to hide.\n');
+    buffer += '✓ 0 errors, [esc] to hide.\n';
   }
+
+  console.log(buffer);
 };
