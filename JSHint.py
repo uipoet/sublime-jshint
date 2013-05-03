26853e35d66444f27b20ff0610fe985f7f04e72b
FichteFoll <fichtefoll2@googlemail.com>
2013-05-03 20:15:11 +0300

    Rewrite JshintCommand entirely
    This version does not require to change the currently active build system since it calls the build by itself.

--- JSHint.py
+++ JSHint.py
@@ -1,8 +1,26 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 
-class JshintCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    self.window.run_command('set_build_system', {
-      'file': 'Packages/JSHint/JSHint.sublime-build'
-    })
-    self.window.run_command('build')
+
+class JshintCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filepath = self.view.file_name()
+        packages = sublime.packages_path()
+        args = {
+            "cmd": [
+                "jshint",
+                filepath,
+                "--reporter",
+                packages + "/JSHint/reporter.js"
+            ],
+            "file_regex": r"JSHint: (.+)\]",
+            "line_regex": r"(\d+),(\d+): (.*)$"
+        }
+
+        if sublime.platform() == "windows":
+            args['cmd'][0] += ".cmd"
+            args['cmd'][3] = args['cmd'][3].replace("/", "\\")
+        if sublime.platform() == "osx":
+            args['path'] = "/usr/local/share/npm/bin:/usr/local/bin:/opt/local/bin"
+
+        self.view.window().run_command('exec', args)
